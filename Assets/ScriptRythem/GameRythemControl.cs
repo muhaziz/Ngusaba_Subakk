6675ced869a762d2f0236c5c7b6c55020d595d16
muhammad aziz <azizkeren2004@gmail.com>
2023-08-19 23:43:55 +0700

    ll\

--- Assets/ScriptRythem/GameRythemControl.cs
+++ Assets/ScriptRythem/GameRythemControl.cs
@@ -1,15 +1,96 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameRythemControl : MonoBehaviour
 {
     public static GameRythemControl instance;
-    public int score;
-    public GameObject resultImage; // Drag and drop ResultImage dari hierarchy ke sini melalui inspector
-    public float gameTime = 60f; // contoh durasi game 60 detik
+    public enum GameState
+    {
+        Tutorial,
+        Playing,
+        Paused,
+        Ended
+    }
+    public GameState currentState = GameState.Tutorial;
+
+    [Header("Game Duration & Score")]
+    public float gameTime = 60.0f;
+    private float currentTime;
+    public int initialScore = 5;
+    public int maxScore = 10;
+    public GameObject resultImage;
+
     private bool gameEnded = false;
+
+    [Header("UI Components")]
+    public Image resultImageUI;  // Kita tambahkan ini dari GameRythemControl
+    public TextMeshProUGUI timerText;
+    public Slider scoreSlider;
+    public GameObject resultMenu;
+    public TextMeshProUGUI scoreText, highscoreText, ratingText;
+
+    [Header("Score & Feedback")]
+    public float sliderDecreaseRate = 0.05f;
+    public float sliderIncreaseRate = 0.05f;
+    public float sliderLerpSpeed = 5.0f;
+    private float intendedSliderValue;
+
+    private bool gameStarted = false;
+
+    private void Start()
+    {
+        if (currentState == GameState.Tutorial)
+        {
+            gameStarted = false;
+            return;
+        }
+
+        BeginGame();
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);  // Kita tambahkan ini dari GameRythemControl
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void BeginGame()
+    {
+        gameStarted = true;
+
+        score = initialScore;
+        intendedSliderValue = score;
+        scoreSlider.maxValue = maxScore;
+        scoreSlider.value = score;
+        currentTime = gameTime;
+        UpdateTimerDisplay();
+
+        StartCoroutine(ConstantDecrease());
+        StartCoroutine(GameTimer());
+    }
 
     private void Update()
     {
+        if (!gameStarted)
+            return;
+
+        intendedSliderValue -= sliderDecreaseRate * Time.deltaTime;
+        scoreSlider.value = Mathf.MoveTowards(scoreSlider.value, intendedSliderValue, sliderLerpSpeed * Time.deltaTime);
+
+        if (scoreSlider.value <= 0 && currentState == GameState.Playing && !gameEnded)
+        {
+            EndGame();
+        }
+
+        // Kode dari GameRythemControl
         if (!gameEnded)
         {
             gameTime -= Time.deltaTime;
@@ -21,34 +102,84 @@ public class GameRythemControl : MonoBehaviour
         }
     }
 
-    void EndGame()
+    public void UpdateScore(int value)
     {
-        gameEnded = true;
-        ShowResult();
-    }
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
+        score += value;
+        score = Mathf.Clamp(score, 0, maxScore);
+        AdjustSliderValue(value * sliderIncreaseRate);
+        scoreText.text = "Score: " + score;
 
-        DontDestroyOnLoad(gameObject);
-    }
-
-    public void UpdateScore(int amount)
-    {
-        score += amount;
+        // Kita tambahkan ini dari GameRythemControl
         Debug.Log("Current Score: " + score);
     }
+
+    void AdjustSliderValue(float amount)
+    {
+        intendedSliderValue += amount;
+        intendedSliderValue = Mathf.Clamp(intendedSliderValue, 0, maxScore);
+    }
+
+    IEnumerator ConstantDecrease()
+    {
+        while (true)
+        {
+            AdjustSliderValue(-sliderDecreaseRate);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    IEnumerator GameTimer()
+    {
+        while (currentTime > 0 && currentState == GameState.Playing)
+        {
+            yield return new WaitForSeconds(1.0f);
+            currentTime--;
+            UpdateTimerDisplay();
+        }
+        EndGame();
+    }
+
+    private void UpdateTimerDisplay()
+    {
+        int minutes = (int)currentTime / 60;
+        int seconds = (int)currentTime % 60;
+        timerText.text = string.Format("{0:D2}:{1:D2}", minutes, seconds);
+    }
+
+    public void StartGameAfterTutorial()
+    {
+        currentState = GameState.Playing;
+        BeginGame();
+    }
+
+    private void EndGame()
+    {
+        if (gameEnded) return;
+
+        gameEnded = true;
+        StopCoroutine("ConstantDecrease");
+        StopAllCoroutines();
+
+        resultMenu.SetActive(true);
+
+        int highscore = PlayerPrefs.GetInt("Highscore", 0);
+        if (score > highscore)
+        {
+            highscore = score;
+            PlayerPrefs.SetInt("Highscore", highscore);
+        }
+        highscoreText.text = "Rekor Terbaik: " + highscore;
+
+        if (score >= 10) ratingText.text = "Rating: A";
+        else if (score >= 7) ratingText.text = "Rating: B";
+
+        // Kita tambahkan ini dari GameRythemControl
+        ShowResult();
+    }
+
     void ShowResult()
     {
-        resultImage.SetActive(true);
+        resultImageUI.SetActive(true);
         // Anda bisa menambahkan lebih banyak detail seperti skor, dsb. di sini
     }
 }
--- Assets/cutsceneManager.cs
+++ Assets/cutsceneManager.cs
@@ -9,7 +9,8 @@ public class cutsceneManager : MonoBehaviour
 
     [SerializeField] private VideoPlayer cutsceneVideo;
     [SerializeField] private Button skipButton;
-
+    public Canvas mainUICanvas; // Drag and drop MainUICanvas dari Inspector
+    public Canvas cutsceneCanvas; // Drag and drop CutsceneCanvas dari Inspector
     private bool hasPlayedCutscene = false;
 
     // Dijalankan oleh CutsceneTrigger
@@ -17,6 +18,12 @@ public class cutsceneManager : MonoBehaviour
     {
         if (!hasPlayedCutscene)
         {
+            // Nonaktifkan Main UI Canvas
+            mainUICanvas.gameObject.SetActive(false);
+
+            // Aktifkan Cutscene UI Canvas (jika Anda memerlukannya)
+            cutsceneCanvas.gameObject.SetActive(true);
+
             cutsceneVideo.Play();
             skipButton.gameObject.SetActive(true);
             skipButton.onClick.AddListener(SkipCutscene);
@@ -35,6 +42,13 @@ public class cutsceneManager : MonoBehaviour
     {
         skipButton.gameObject.SetActive(false);
         skipButton.onClick.RemoveListener(SkipCutscene);
+
+        // Aktifkan kembali Main UI Canvas
+        mainUICanvas.gameObject.SetActive(true);
+
+        // Nonaktifkan Cutscene UI Canvas
+        cutsceneCanvas.gameObject.SetActive(false);
+
         // Anda mungkin ingin menambahkan logika lain di sini, misalnya menonaktifkan trigger.
     }
 }
