c970a01629f484933e068b46584473bb4449ce11
muhammad aziz <azizkeren2004@gmail.com>
2023-08-20 19:44:28 +0700

    add

---
+++ Assets/videotoendd.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class videotoendd : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string nextScene;
+
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += LoadScene;
+    }
+
+    void LoadScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
